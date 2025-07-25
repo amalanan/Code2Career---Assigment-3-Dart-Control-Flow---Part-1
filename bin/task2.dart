29aa3ee8736dd56932674893b3957ec951e30592
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 19:12:28 +0300

    Update task2.dart

--- bin/task2.dart
+++ bin/task2.dart
@@ -1,6 +1,23 @@
 import 'dart:io';
 void main() {
   // Task 2 FOR Loop – Even Numbers 
+ print('Printing Even numbers from 1 to 20:');
+  for (int i = 1; i <= 20; i++) {
+    if (i % 2 != 0) {
+      continue;
+    }
+    print(i);
+  }
+   // orrrrrrrrrrrrr
+ print('Printing even numbers from 1 to 20:');
+  for (int i = 1; i <= 20; i++) {
+    for (int j = 0; j < 1; j++) {
+      if (i % 2 == 0) {
+        stdout.write('$i\n');
+      }
+    }
+  }
+   // orrrrrrrrrrrrr
   print('Printing Even numbers from 1 to 20:'); 
   for (int i = 1; i <= 20; i++) {
     if (i % 2 == 0) {
